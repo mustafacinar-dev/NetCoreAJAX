88f5544b6ed15c957a1df3614e88ab8bdbf90521
Mustafa ÇINAR <mustafa.cinar7@hotmail.com>
2020-09-21 04:25:05 +0300

    Arayüz kısmında son düzenlemeler yapıldı.

--- NetCoreAJAX/Controllers/HomeController.cs
+++ NetCoreAJAX/Controllers/HomeController.cs
@@ -38,16 +38,5 @@ namespace NetCoreAJAX.Controllers
             UserDb.AddUser(user);
             return Json(user);
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
