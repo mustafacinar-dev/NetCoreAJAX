92436d20c43c8f41af93eac8a176f52e033683e6
Mustafa ÇINAR <mustafa.cinar7@hotmail.com>
2020-09-21 03:57:28 +0300

    Add project files.

---
+++ NetCoreAJAX/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetCoreAJAX.Models;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NetCoreAJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            //Arayüzdeki loader componentinin gözükmesi için threadi uyuttum çünkü, normal şartlarda işlem milisaniyeler içerisinde tamamlanıyor.
+            Thread.Sleep(2000);
+            return Json(UserDb.List());
+        }
+
+        public IActionResult GetUser(int id)
+        {
+            var user = UserDb.GetUser(id);
+            return Json(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            UserDb.AddUser(user);
+            return Json(user);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NetCoreAJAX/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NetCoreAJAX.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public static class UserDb
+    {
+        private static List<User> Users = new List<User>()
+        {
+            new User{Id=1,Name="Atiba"},
+            new User{Id=2,Name="Oğuzhan"},
+            new User{Id=3,Name="Mensah"},
+            new User{Id=4,Name="Ljajic"},
+            new User{Id=5,Name="Dorukhan"},
+            new User{Id=6,Name="Necip"}
+        };
+
+        public static List<User> List()
+        {
+            return Users;
+        }
+
+        public static User GetUser(int id)
+        {
+            return Users.FirstOrDefault(p => p.Id == id);
+        }
+
+        public static void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+    }
+}
